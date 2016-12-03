715bc0e8639da20dc851637a9a0c1062b122fccf
Brady Ericksen <bradyericksen@gmail.com>
2016-12-02 19:26:26 -0700

    Here we go. Started SecondBrain.py

---
+++ src/SecondBrain.py
@@ -0,0 +1,13 @@
+'''
+Created on Dec 2, 2016
+
+@author: Brady
+'''
+
+print "Welcome to Brady's Second Brain. This is where he keeps everything that will eventually leak out of the primary brain."
+print "Feel free to use it in a similar way."
+print "Please enter a command:"
+print "1. Create a Brain Tissue file"
+print "2. Read a Brain Tissue file"
+print "3. Help (AKA What the hell is this?"
+print "4. Exit"
