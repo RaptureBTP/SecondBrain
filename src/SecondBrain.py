e55cc2ccdf8c1bfdf9a367b6fdbf5cc755f437c1
Brady Ericksen <bradyericksen@gmail.com>
2016-12-02 20:12:18 -0700

    Started filling in the menu and process of reading a BrainTissue file

--- src/SecondBrain.py
+++ src/SecondBrain.py
@@ -3,11 +3,38 @@ Created on Dec 2, 2016
 
 @author: Brady
 '''
+from __builtin__ import file, exit
 
-print "Welcome to Brady's Second Brain. This is where he keeps everything that will eventually leak out of the primary brain."
+inUse = True
+
+print "Welcome to Brady's Second Brain. This is where he keeps everything that will eventually leak out of his primary brain."
 print "Feel free to use it in a similar way."
-print "Please enter a command:"
-print "1. Create a Brain Tissue file"
-print "2. Read a Brain Tissue file"
-print "3. Help (AKA What the hell is this?"
-print "4. Exit"
+
+while inUse == True:
+    print "Options:"
+    print "1. Create a Brain Tissue file"
+    print "2. Read a Brain Tissue file"
+    print "3. Edit a Brain Tissue file"
+    print "4. Help (AKA What the hell is this?"
+    print "5. Exit"
+    
+    response = raw_input("Please enter a command: ")
+    if response == 1 or response == 2 or response == 3 or response == 4:
+        if response == 1:
+            print "Okay I'll get to this. Gotta logic through how the file's will built up."
+        elif response == 2:
+                fileLocation = raw_input("Please enter absolute file path: ")
+                tissue = open(fileLocation, 'r')
+        
+                if tissue.readline() != "[BrainTissue]":
+                    print "Not a Brain Tissue file."
+                    for line in tissue:
+                        print line #temp
+        elif response == 3:
+            print "time to edit a file"
+        elif response == 4:
+            print "Explain shit here"
+        elif response == 5:
+            exit
+        else:
+            print "Invalid response. Please try again."
